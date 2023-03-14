013d41618af65a2a84bd039ab6a50a0c348bef6d
Chuck Smith <csmit223@nd.edu>
2023-03-14 17:14:11 -0400

    Update messaging to use Bootstrap toast

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+import * as bootstrap from "bootstrap";
+
+export default class extends Controller {
+    connect() {
+        new bootstrap.Toast(this.element).show();
+    }
+}
